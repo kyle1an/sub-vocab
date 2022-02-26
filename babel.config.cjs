0ee90568f578204fea0dc845bcf85f09699d183f
Mitchell <50689806+kyle1an@users.noreply.github.com>
2022-02-27 00:28:28 +0800

    Update

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,10 +1,5 @@
 module.exports = {
     presets: [
-        '@vue/cli-plugin-babel/preset',
-        {
-            targets: {
-                node: 'current'
-            }
-        }
+        '@vue/cli-plugin-babel/preset'
     ]
 }
