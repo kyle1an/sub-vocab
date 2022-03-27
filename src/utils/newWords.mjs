17b7b22d89f59629c45eb5de18148d88aa882163
Mitchell <50689806+kyle1an@users.noreply.github.com>
2022-03-27 15:02:09 +0800

    Update words filter

--- src/utils/newWords.mjs
+++ src/utils/newWords.mjs
@@ -1,18 +1,31 @@
 import fs from "fs";
 
+const pathCommon = '../../public/common-words.txt'
 const pathMy = '../../public/myWords.txt'
 const pathNew = '../../public/newWords.txt'
 
-const wordsNew = fs.readFileSync(pathNew, 'utf8')
+const common = fs.readFileSync(pathCommon, 'utf8').match(/[A-Za-z]+(?:['-]?[A-Za-z]'?)+/mg)
+const myWords = fs.readFileSync(pathMy, 'utf8').match(/[A-Za-z]+(?:['-]?[A-Za-z]'?)+/mg)
+const wordsNew = fs.readFileSync(pathNew, 'utf8').match(/[A-Za-z]+(?:['-]?[A-Za-z]'?)+/mg) || ''
+
 console.log(wordsNew)
 
-reorder(pathMy);
+reorder(pathNew);
 
 function reorder(path) { //   sieve.match(/[A-Za-z]+(?:['-]?[A-Za-z]'?)+/mg
-    const words = fs.readFileSync(path, 'utf8')
-    const wList = [...new Set(words.match(/[A-Za-z]+(?:['-]?[A-Za-z]'?)+/mg))].sort().join('\n');
-    console.log(wList)
-    fs.writeFile(path, wList, (err) => {
+    const commonSet = new Set(common)
+    const currentSet = new Set(myWords)
+    const neW = [];
+    for (const w of wordsNew) {
+        if (!commonSet.has(w) && !currentSet.has(w))
+            neW.push(w)
+    }
+    const res = [...new Set(myWords.concat(neW))]
+    fs.writeFile(pathMy, res.sort().join('\n'), (err) => {
+        if (err) return console.log(err);
+        console.log("The file was saved!");
+    });
+    fs.writeFile(path, '', (err) => {
         if (err) return console.log(err);
         console.log("The file was saved!");
     });
