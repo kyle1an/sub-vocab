5a9e9f3a8c6f226bf9717cc994906f3584d38c6a
Mitchell <50689806+kyle1an@users.noreply.github.com>
2022-05-23 00:11:38 +0800

    Update: prevent the irregulars from being extracted

--- src/utils/shortWordsSuffixMapping.ts
+++ src/utils/shortWordsSuffixMapping.ts
@@ -1,13 +1,30 @@
-export const STEMS: Array<any> = [
-  ['ad', 'ads'],
-  ['be', 'being', 'been'],
-  ['do', 'doing', 'does', 'done'],
-  ['have', 'has', 'having', 'had'],
-  ['is'],
-  ['not'],
-  ['shed'],
-  ['th'],
-  ['the'],
-  ['us'],
-  ['we', "we'll", "we've",]
+export const EXTRACT: Array<any> = [
+  ["ad", "ads"],
+  ["com",],
+  ["is"],
+  ["not"],
+  ["shed"],
+  ["th"],
+  ["the"],
+  ["us"],
+  ["we"],
+]
+
+export const IRREGULAR: Array<any> = [
+  ["are", "aren't", "ain't", "were", "weren't"],
+  ["be", "been"],
+  ["begin", "began", "begun", "beginning"],
+  ["can", "can't", "couldn't"],
+  ["come", "came",],
+  ["dare", "daren't"],
+  ["do", "done", "don't", "doesn't", "didn't"],
+  ["have", "has", "had", "haven't", "hasn't", "hadn't"],
+  ["is", "was", "isn't", "wasn't",],
+  ["might", "mightn't"],
+  ["must", "mustn't"],
+  ["need", "needn't"],
+  ["ought", "oughtn't"],
+  ["shall", "shan't", "should", "shouldn't"],
+  ["we", "we'll", "we've",],
+  ["will", "won't", "would", "wouldn't"]
 ]
