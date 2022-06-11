55d4290a592759f9465a5c2b40c45d61817cb586
Mitchell <50689806+kyle1an@users.noreply.github.com>
2022-06-11 11:04:19 +0800

    New: merge words end with -ies, -ied & double consonant -ing

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -18,8 +18,7 @@ function submitForm() {
   <div class="common-layout">
     <el-container>
       <el-main class="mx-auto">
-        <el-card shadow="always" class="w-96 flex justify-center">
-          <div style="margin: 20px" />
+        <el-card shadow="always" class="w-80 flex justify-center mx-auto">
           <el-form
             label-position="top"
             label-width="100px"
@@ -29,15 +28,14 @@ function submitForm() {
               <el-input v-model="formData.name" />
             </el-form-item>
             <el-form-item class="font-bold" label="Password">
-              <el-input v-model="formData.password" type="password" show-password />
+              <el-input v-model="formData.password" type="password" />
             </el-form-item>
           </el-form>
           <el-form-item>
-            <el-button type="primary" @click="submitForm(ruleFormRef)">Log in</el-button>
+            <el-button class="w-full" type="primary" @click="submitForm()">Log in</el-button>
           </el-form-item>
         </el-card>
       </el-main>
-      <el-footer>Footer</el-footer>
     </el-container>
   </div>
 </template>
