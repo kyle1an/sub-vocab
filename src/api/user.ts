bc15238e0d223f6cce84c41745a5d29a0e122ae4
Mitchell <50689806+kyle1an@users.noreply.github.com>
2022-06-25 17:08:39 +0800

    New: user register

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,7 +1,13 @@
-import { fetchPost  } from './request';
+import { fetchPost } from './request';
 
 export async function login(info: { username: string, password: string }) {
   return fetchPost(`${import.meta.env.VITE_SUB_PROD}/login`, {
+    body: JSON.stringify(info),
+  })
+}
+
+export async function register(info: { username: string, password: string }) {
+  return fetchPost(`${import.meta.env.VITE_SUB_PROD}/register`, {
     body: JSON.stringify(info),
   })
 }
