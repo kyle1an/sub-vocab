02f670cf1aebdf57095d839f82188696de6f5478
Mitchell <50689806+kyle1an@users.noreply.github.com>
2022-06-28 23:06:50 +0800

    feat: change username/password

--- src/api/request.ts
+++ src/api/request.ts
@@ -6,6 +6,7 @@ export function fetchPost(url: string, options: RequestInit = {}) {
   return fetch(url, {
     method: 'post',
     headers,
+    credentials: 'include',
     ...options
   })
     .then(response => response.json())
--- src/views/user.vue
+++ src/views/user.vue
@@ -3,12 +3,127 @@ import { useUserStore } from '../store/useState';
 import { eraseCookie } from '../utils/cookie';
 import router from '../router';
 
+import { reactive, ref } from 'vue'
+import type { FormInstance } from 'element-plus'
+import { changePassword, changeUsername, login } from '../api/user';
+
+const store = useUserStore()
+const ruleFormRef = ref<FormInstance>()
+
+function checkUsername(rule: any, value: any, callback: any) {
+  const username = String(value)
+  if (!username.length) {
+    return callback(new Error('Please input name'))
+  }
+
+  if (username.length > 20) {
+    return callback(new Error('Please use a shorter name'))
+  }
+
+  if (username.length < 2) {
+    return callback(new Error('Name must be longer than 1'))
+  }
+
+  callback()
+}
+
+function validatePass(rule: any, value: any, callback: any) {
+  if (value === '') {
+    return callback();
+  }
+
+  if (ruleForm.value.checkPass !== '') {
+    if (!ruleFormRef.value) return
+    ruleFormRef.value.validateField('checkPass', () => null)
+  }
+  callback()
+}
+
+const validatePass2 = (rule: any, value: any, callback: any) => {
+  if (value === '' && ruleForm.value.password === '') {
+    return callback()
+  }
+
+  if (value !== ruleForm.value.password) {
+    callback(new Error("Two inputs don't match!"))
+  } else {
+    callback()
+  }
+}
+
+const ruleForm = ref({
+  username: '',
+  oldPassword: '',
+  password: '',
+  checkPass: '',
+})
+
+const rules = reactive({
+  username: [{ validator: checkUsername, trigger: 'blur' }],
+  password: [{ validator: validatePass, trigger: 'blur' }],
+  checkPass: [{ validator: validatePass2, trigger: 'blur' }],
+})
+
+function submitForm(formEl: FormInstance | undefined) {
+  if (!formEl) return
+  formEl.validate((valid) => {
+    if (valid) {
+      console.log('submit!', formEl)
+      alterInfo(ruleForm.value)
+    } else {
+      console.log('error submit!')
+      return false
+    }
+  })
+}
+
+const errorMsg = ref('')
+
+async function alterInfo(form: any) {
+  if (form.password !== '') {
+    form.password = form.password.trim()
+    const res = await changePassword({
+      username: form.username,
+      oldPassword: form.oldPassword,
+      newPassword: form.password,
+    })
+
+    if (res.success) {
+      logOut()
+    } else {
+      errorMsg.value = res.message || 'something went wrong'
+    }
+    console.log(res)
+  }
+
+  if (form.username !== '' && form.username !== store.user.name) {
+    form.username = form.username.trim()
+    const res = await changeUsername({
+      username: store.user.name,
+      newUsername: form.username,
+    })
+
+    if (res.success) {
+      store.user.name = form.username
+    } else {
+      errorMsg.value = res.message || 'something went wrong'
+    }
+  }
+}
+
+function resetForm(formEl: FormInstance | undefined) {
+  if (!formEl) return
+  formEl.resetFields()
+}
+
 const userStore = useUserStore()
 
 function logOut() {
   eraseCookie('user')
   userStore.user.name = ''
-  router.push('/login')
+  setTimeout(() => {
+    router.push('/login')
+  }, 0)
 }
 </script>
 
@@ -17,12 +132,44 @@ function logOut() {
     <el-container>
       <el-main class="mx-auto">
         <el-card shadow="always" class="w-80 flex justify-center mx-auto">
-          <el-button @click="logOut">log out</el-button>
+          <el-form
+            ref="ruleFormRef"
+            :model="ruleForm"
+            :rules="rules"
+            label-position="top"
+            label-width="100px"
+            class="demo-ruleForm"
+            style="max-width: 460px"
+            status-icon
+          >
+            <el-form-item label="Name" prop="username" :error="errorMsg">
+              <el-input v-model.number="ruleForm.username" class="!text-base md:!text-xs" />
+            </el-form-item>
+            <el-form-item label="Old Password" prop="oldPassword">
+              <el-input v-model="ruleForm.oldPassword" type="password" autocomplete="off" class="!text-base md:!text-xs" />
+            </el-form-item>
+            <el-form-item label="New Password" prop="password">
+              <el-input v-model="ruleForm.password" type="password" autocomplete="off" class="!text-base md:!text-xs" />
+            </el-form-item>
+            <el-form-item label="New Password Confirm" prop="checkPass">
+              <el-input v-model="ruleForm.checkPass" type="password" autocomplete="off" class="!text-base md:!text-xs" />
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="submitForm(ruleFormRef)">Confirm Changes</el-button>
+              <el-button @click="resetForm(ruleFormRef)">Reset</el-button>
+            </el-form-item>
+          </el-form>
         </el-card>
+        <div class="flex justify-center pt-8">
+          <el-button @click="logOut">log out</el-button>
+        </div>
       </el-main>
     </el-container>
   </div>
 </template>
 
 <style scoped>
+:deep(.el-form-item label) {
+  font-weight: bold;
+}
 </style>
