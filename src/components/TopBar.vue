acdbb3bafd04c1798643a81408080b533494cf86
Mitchell <50689806+kyle1an@users.noreply.github.com>
2022-07-01 23:18:50 +0800

    style: top bar menu background

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -10,24 +10,24 @@ const isWide = window.innerWidth >= 460
 
 <template>
   <el-header class="top-bar flex w-full z-50 items-center justify-center relative min-w-[auto]">
-    <nav class="flex w-full box-border justify-center max-w-[600px]">
+    <nav class="flex w-full h-full box-border justify-center max-w-[600px]">
       <div class="flex items-center nav-main justify-between w-full ">
-        <el-button type="" text>
+        <el-button type="" text class="!h-full !rounded-none">
           <router-link to="/">Home</router-link>
         </el-button>
         <el-button type="" text class="!rounded-full">
           <router-link to="/about">About</router-link>
         </el-button>
-        <el-button v-show="username||isWide" type="primary" text>
+        <el-button v-show="username||isWide" type="primary" text class="!h-full !rounded-none">
           <router-link to="/mine">{{ acquaintedSection }}</router-link>
         </el-button>
-        <el-button v-show="username" type="" text>
+        <el-button v-show="username" type="" text class="!h-full !rounded-none">
           <router-link to="/user">{{ username }}</router-link>
         </el-button>
-        <el-button v-show="!username" type="primary" plain>
+        <el-button v-show="!username" type="primary" plain class="!px-[13px]">
           <router-link to="/login">Log in</router-link>
         </el-button>
-        <el-button v-show="!username" type="primary">
+        <el-button v-show="!username" type="primary" class="!px-[13px]">
           <router-link to="/register">Sign up</router-link>
         </el-button>
       </div>
@@ -40,5 +40,9 @@ const isWide = window.innerWidth >= 460
   box-shadow: var(--bs-sm);
   background-color: var(--theme-topbar-background-color);
   height: var(--theme-topbar-height) !important;
+
+  :deep([href]) {
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  }
 }
 </style>
