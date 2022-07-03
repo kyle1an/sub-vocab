616fb68640a6c156fe42f080785a9367c35cf6a6
Mitchell <50689806+kyle1an@users.noreply.github.com>
2022-07-03 16:33:04 +0800

    feat: add internationalization

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,24 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import { createPinia } from 'pinia';
 import router from './router'
+import { getCookie } from './utils/cookie';
+import { createI18n } from 'vue-i18n'
+import { messages } from './utils/localeMessages';
+
+const i18n = createI18n({
+  locale: getCookie('lang') || 'zh', // set locale
+  fallbackLocale: 'en', // set fallback locale
+  messages, // set locale messages
+})
 
 const app = createApp(App)
 app.use(ElementPlus)
 app.use(createPinia())
 // Make sure to _use_ the router instance to make the whole app router-aware.
 app.use(router)
-
+app.use(i18n)
 // app.config.productionTip = false
 // app.config.devtools = false
 
 app.mount('#app')
+
--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,17 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   define: {
-    __VUE_OPTIONS_API__: false,
+    // __VUE_OPTIONS_API__: false,
   },
   build: {
     target: 'esnext'
+  },
+  resolve: {
+    alias: [
+      {
+        find: 'vue-i18n',
+        replacement: 'vue-i18n/dist/vue-i18n.cjs.js',
+      }
+    ]
   }
 })
