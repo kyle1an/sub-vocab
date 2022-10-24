e1d04d2ea6efb0208429d3b417b62a16096c67c8
Mitchell <50689806+kyle1an@users.noreply.github.com>
2022-10-24 23:48:29 +0800

    chore(deps): bump express from 4.18.1 to 4.18.2

--- config/connection.ts
+++ config/connection.ts
@@ -2,13 +2,12 @@ import dotenv from 'dotenv'
 import mysql from 'mysql2'
 
 dotenv.config()
-const config = {
+
+export const pool = mysql.createPool({
   connectionLimit: 10,
   host: process.env.HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DATABASE,
   multipleStatements: true,
-}
-const pool = mysql.createPool(config)
-export { pool }
+})
