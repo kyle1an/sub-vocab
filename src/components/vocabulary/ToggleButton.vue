a5a1f264c86f8f8a90d682e6482fd75f31b1baec
Mitchell <50689806+kyle1an@users.noreply.github.com>
2022-12-26 22:52:27 +0800

    refactor: keep propagation of button click event

--- src/components/vocabulary/ToggleButton.vue
+++ src/components/vocabulary/ToggleButton.vue
@@ -1,6 +1,4 @@
 <script lang="tsx" setup>
-import { Check, Loading } from '@element-plus/icons-vue'
-import { ElButton, ElIcon } from 'element-plus'
 import type { Sieve } from '@/types'
 
 const {
@@ -13,19 +11,32 @@ const {
 </script>
 
 <template>
-  <ElButton
-    color="#facc15"
-    size="small"
+  <button
     :disabled="row.inUpdating"
-    :class="`${row.acquainted?'!text-white':'un !border-zinc-300 !bg-transparent !text-transparent hover:!text-black'} ${row.inUpdating?'[&_.is-loading]:!inline-flex [&_.check]:hidden [.un&]:!text-black':''}`"
-    circle
-    @click.stop="handleVocabToggle(row)"
+    :class="`el-button !p-[5px] !text-xs !w-6 !h-6 !rounded-[50%] ${row.acquainted?'!bg-[#facc15] focus:!bg-[rgb(252,219,91)] hover:!bg-[rgb(252,219,91)] !border-[#facc15] !text-white':'un !border-zinc-300 !bg-transparent !text-transparent hover:!text-black'} ${row.inUpdating?'[.un&]:!text-black':''}`"
+    @click="handleVocabToggle(row)"
   >
-    <ElIcon class="is-loading !hidden">
-      <Loading />
-    </ElIcon>
-    <ElIcon class="check">
-      <Check />
-    </ElIcon>
-  </ElButton>
+    <i :class="`el-icon animate-[rotating_2s_linear_infinite] ${row.inUpdating?'':'!hidden'}`">
+      <svg
+        viewBox="0 0 1024 1024"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill="currentColor"
+          d="M512 64a32 32 0 0 1 32 32v192a32 32 0 0 1-64 0V96a32 32 0 0 1 32-32zm0 640a32 32 0 0 1 32 32v192a32 32 0 1 1-64 0V736a32 32 0 0 1 32-32zm448-192a32 32 0 0 1-32 32H736a32 32 0 1 1 0-64h192a32 32 0 0 1 32 32zm-640 0a32 32 0 0 1-32 32H96a32 32 0 0 1 0-64h192a32 32 0 0 1 32 32zM195.2 195.2a32 32 0 0 1 45.248 0L376.32 331.008a32 32 0 0 1-45.248 45.248L195.2 240.448a32 32 0 0 1 0-45.248zm452.544 452.544a32 32 0 0 1 45.248 0L828.8 783.552a32 32 0 0 1-45.248 45.248L647.744 692.992a32 32 0 0 1 0-45.248zM828.8 195.264a32 32 0 0 1 0 45.184L692.992 376.32a32 32 0 0 1-45.248-45.248l135.808-135.808a32 32 0 0 1 45.248 0zm-452.544 452.48a32 32 0 0 1 0 45.248L240.448 828.8a32 32 0 0 1-45.248-45.248l135.808-135.808a32 32 0 0 1 45.248 0z"
+        />
+      </svg>
+    </i>
+    <i :class="`el-icon ${row.inUpdating?'!hidden':''}`">
+      <svg
+        viewBox="0 0 1024 1024"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill="currentColor"
+          d="M406.656 706.944 195.84 496.256a32 32 0 1 0-45.248 45.248l256 256 512-512a32 32 0 0 0-45.248-45.248L406.592 706.944z"
+        />
+      </svg>
+    </i>
+  </button>
 </template>
