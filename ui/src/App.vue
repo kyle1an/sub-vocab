8c0156ac1c27d843a7d831a0062c0fa136283de0
Mitchell <50689806+kyle1an@users.noreply.github.com>
2023-02-03 23:38:49 +0800

    refactor: add unique id to input in component

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -3,21 +3,23 @@ import TopBar from '@/components/TopBar.vue'
 </script>
 
 <template>
-  <TopBar class="ffs-pre fixed h-12" />
-  <div class="ffs-pre flex min-h-[100vh] flex-col items-center pt-12">
-    <RouterView v-slot="{ Component, route }">
-      <KeepAlive>
+  <div class="flex h-full min-h-full flex-col bg-white antialiased">
+    <TopBar class="ffs-pre fixed h-12" />
+    <div class="ffs-pre flex min-h-[100vh] flex-col items-center pt-12">
+      <RouterView v-slot="{ Component, route }">
+        <KeepAlive>
+          <component
+            :is="Component"
+            v-if="route.meta.keepAlive"
+            :key="route.path"
+          />
+        </KeepAlive>
         <component
           :is="Component"
-          v-if="route.meta.keepAlive"
+          v-if="!route.meta.keepAlive"
           :key="route.path"
         />
-      </KeepAlive>
-      <component
-        :is="Component"
-        v-if="!route.meta.keepAlive"
-        :key="route.path"
-      />
-    </RouterView>
+      </RouterView>
+    </div>
   </div>
 </template>
--- ui/src/components/FileInput.vue
+++ ui/src/components/FileInput.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { v4 as uuidv4 } from 'uuid'
 import { readFiles, resetFileInput } from '@/utils/utils'
 
 const emit = defineEmits(['fileInput'])
@@ -16,18 +17,19 @@ async function onFileChange(ev: Event) {
 
 const inputChanged = () => resetFileInput('.file-input')
 defineExpose({ inputChanged })
+const id = uuidv4()
 </script>
 
 <template>
   <div>
     <label
       class="box-border inline-flex h-8 max-h-full grow-0 cursor-pointer items-center justify-center whitespace-nowrap rounded-md border bg-white px-3 py-2.5 text-center align-middle text-sm leading-3 tracking-wide text-neutral-800 transition-colors hover:border-sky-300 hover:bg-sky-100 hover:text-sky-600"
-      for="browseFiles"
+      :for="'browseFiles'+id"
     >
       <slot />
     </label>
     <input
-      id="browseFiles"
+      :id="'browseFiles'+id"
       class="file-input"
       type="file"
       hidden
